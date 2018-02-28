cc78e54c4ff2dd4990ccf6900f36fe68dd361b60
Olivier Matz <zer0@droids-corp.org>
2018-02-28 20:58:58 +0100

    fix typos in str node test

--- lib/ecoli_node_str.c
+++ lib/ecoli_node_str.c
@@ -222,20 +222,16 @@ static int ec_node_str_testcase(void)
 		"");
 	ret |= EC_TEST_CHECK_COMPLETE(node,
 		"", EC_NODE_ENDLIST,
-		"foo", EC_NODE_ENDLIST,
-		"foo");
+		"foo", EC_NODE_ENDLIST);
 	ret |= EC_TEST_CHECK_COMPLETE(node,
 		"f", EC_NODE_ENDLIST,
-		"foo", EC_NODE_ENDLIST,
-		"foo");
+		"foo", EC_NODE_ENDLIST);
 	ret |= EC_TEST_CHECK_COMPLETE(node,
 		"foo", EC_NODE_ENDLIST,
-		"foo", EC_NODE_ENDLIST,
-		"foo");
+		"foo", EC_NODE_ENDLIST);
 	ret |= EC_TEST_CHECK_COMPLETE(node,
 		"x", EC_NODE_ENDLIST,
-		EC_NODE_ENDLIST,
-		"");
+		EC_NODE_ENDLIST);
 	ec_node_free(node);
 
 	return ret;
