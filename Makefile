vjardin/libecoli

commit dc86223186d320a9d2708b3dc045bb684b504bb7

build: add makefile
Add a makefile for developer quality of life. By default `make` will run
meson setup and ninja with sensible arguments.

Include targets for running tests and generating coverage reports.

The 'tests' target runs meson test with error logs printed. The
'coverage' target depends on tests and uses gcovr to generate both HTML
and text coverage reports, excluding test/ and examples/ directories.
The coverage option is passed to meson via -Db_coverage flag.

The coverage report is output to $(BUILDDIR)/coverage/index.html.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Acked-by: Olivier Matz <zer0@droids-corp.org>

Makefile
@@ -1,0 +1,81 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Robin Jarry
+
+BUILDDIR ?= build
+BUILDTYPE ?= debugoptimized
+SANITIZE ?= none
+COVERAGE ?= false
+V ?= 0
+ifeq ($V,1)
+ninja_opts = --verbose
+Q =
+else
+Q = @
+endif
+CC ?= gcc
+
+.PHONY: build
+build: $(BUILDDIR)/build.ninja
+	$Q ninja -C $(BUILDDIR) $(ninja_opts)
+
+.PHONY: debug
+debug: BUILDTYPE = debug
+debug: SANITIZE = address
+debug: COVERAGE = true
+debug: all
+
+.PHONY: tests
+tests: $(BUILDDIR)/build.ninja
+	$Q meson test -C $(BUILDDIR) --print-errorlogs $(if $(filter 1,$V),--verbose)
+
+.PHONY: coverage
+coverage: tests
+	$Q mkdir -p $(BUILDDIR)/coverage
+	gcovr --html-details $(BUILDDIR)/coverage/index.html --txt \
+		-e 'test/*' -e 'examples/*' --gcov-ignore-parse-errors \
+		--gcov-executable `$(CC) -print-prog-name=gcov` \
+		--object-directory $(BUILDDIR) \
+		--sort uncovered-percent \
+		-r . $(BUILDDIR)
+	@echo Coverage data is present in $(BUILDDIR)/coverage/index.html
+
+.PHONY: clean
+clean:
+	$Q ninja -C $(BUILDDIR) clean $(ninja_opts)
+
+meson_opts = --buildtype=$(BUILDTYPE) --werror --warnlevel=2
+meson_opts += -Db_sanitize=$(SANITIZE) -Db_coverage=$(COVERAGE)
+meson_opts += $(MESON_EXTRA_OPTS)
+
+$(BUILDDIR)/build.ninja:
+	meson setup $(BUILDDIR) $(meson_opts)
+
+.PHONY: tag-release
+tag-release:
+	@cur_version=`sed -En "s/^[[:space:]]+version[[:space:]]*:[[:space:]]*'([0-9\.]+)'\\$$/\\1/p" meson.build` && \
+	next_version=`echo $$cur_version | awk -F. -v OFS=. '{$$(NF) += 1; print}'` && \
+	read -rp "next version ($$next_version)? " n && \
+	if [ -n "$$n" ]; then next_version="$$n"; fi && \
+	set -xe && \
+	sed -i "s/\<$$cur_version\>/$$next_version/" meson.build && \
+	git commit -sm "release: version $$next_version" -m "`git shortlog -n v$$cur_version..`" meson.build && \
+	git tag -sm "`git shortlog -n v$$cur_version..HEAD^`" "v$$next_version"
+
+.PHONY: help
+help:
+	$Q echo 'Available targets:'
+	$Q echo
+	$Q echo '  build         Configure and build with default options (default target)'
+	$Q echo '  debug         Configure and build with -O0 and -fsantize=address'
+	$Q echo '  clean         Clean build directory'
+	$Q echo '  tag-release   Create a release commit and signed tag'
+	$Q echo '  tests         Run unit tests'
+	$Q echo '  coverage      Run unit tests and generate test coverage report'
+	$Q echo
+	$Q echo 'Environment variables:'
+	$Q echo
+	$Q echo '  BUILDDIR            Build directory (default: $(BUILDDIR))'
+	$Q echo '  BUILDTYPE           Optimization level (default: $(BUILDTYPE))'
+	$Q echo '  COVERAGE            Enable coverage data in binaries (default: $(COVERAGE))'
+	$Q echo '  SANITIZE            Value of -fsanitize (default: $(SANITIZE))'
+	$Q echo '  MESON_EXTRA_OPTS    Additional options for meson setup'

commit d4363aa6651b5f2c7414323aad3ab6eb4a74e2b6

mk: fix tag-release version detection
The regular expression does not match. Add missing trailing comma and
remove escape on the end-of-line character.

Fixes: dc86223186d3 ("build: add makefile")
Signed-off-by: Robin Jarry <robin@jarry.cc>

Makefile
@@ -97,7 +97,7 @@ git-config:
 
 .PHONY: tag-release
 tag-release:
-	@cur_version=`sed -En "s/^[[:space:]]+version[[:space:]]*:[[:space:]]*'([0-9\.]+)'\\$$/\\1/p" meson.build` && \
+	@cur_version=`sed -En "s/^[[:space:]]+version[[:space:]]*:[[:space:]]*'([0-9\\.]+)',$$/\\1/p" meson.build` && \
 	next_version=`echo $$cur_version | awk -F. -v OFS=. '{$$(NF) += 1; print}'` && \
 	read -rp "next version ($$next_version)? " n && \
 	if [ -n "$$n" ]; then next_version="$$n"; fi && \